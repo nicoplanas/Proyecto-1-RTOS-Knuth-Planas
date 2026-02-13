2c1200664021ccbcaffb574d51e73aec60fcf7e4
nicoplanas <planas.nicolas@correo.unimet.edu.ve>
2026-02-13 16:29:19 -0400

    Primer commit: Vinculación de proyecto y creación de clase ‘DoubleSeries’

---
+++ src/main/java/com/rtosim/Main.java
@@ -0,0 +1,7 @@
+package com.rtosim;
+
+public class Main {
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ src/main/java/com/rtosim/struct/DoubleSeries.java
@@ -0,0 +1,51 @@
+package com.rtosim.struct;
+// Clase 1
+public class DoubleSeries {
+    private double[] data;
+    private int size;
+
+    public DoubleSeries(int capacity) {
+        if (capacity <= 0) {
+            capacity = 32;
+        }
+        data = new double[capacity];
+        size = 0;
+    }
+
+    public void add(double value) {
+        if (size >= data.length) {
+            double[] next = new double[data.length * 2];
+            for (int i = 0; i < data.length; i += 1) {
+                next[i] = data[i];
+            }
+            data = next;
+        }
+        data[size] = value;
+        size += 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public double get(int index) {
+        if (index < 0 || index >= size) {
+            return 0.0;
+        }
+        return data[index];
+    }
+
+    public double max() {
+        double max = 0.0;
+        for (int i = 0; i < size; i += 1) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+        return max;
+    }
+
+    public void clear() {
+        size = 0;
+    }
+}
